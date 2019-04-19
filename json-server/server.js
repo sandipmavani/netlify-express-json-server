716bc8071834c562b2d485da42c5b99f8b6a8ab5
sandip <sandip.mavani@corp.avinashi.com>
2019-04-19 18:29:26 +0530

    minor changes

--- json-server/server.js
+++ json-server/server.js
@@ -15,6 +15,11 @@ router.get('/', (req, res) => {
     res.write('<h1>Hello from Express.js!</h1>');
     res.end();
 });
+router.get('/public/style.css', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.write('');
+    res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
