f50027c2f1d72518cc0ccfd388cc74a3904490b6
sandip <sandip.mavani@corp.avinashi.com>
2019-04-19 17:43:04 +0530

    database file

--- json-server/server.js
+++ json-server/server.js
@@ -3,26 +3,18 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const jsonServer = require('json-server');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from json-server</h1>');
-  res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use('/api', jsonServer.router('db.json'));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
-
-
-
-
-
